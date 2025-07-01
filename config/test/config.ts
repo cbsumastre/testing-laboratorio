9b4a7a915bf65f4c7277dfdfe36ed41e2fd06a34
Carlos Blázquez <admin@firstflip.es>
2025-07-01 18:32:13 +0200

    fixed Error: Playwright Test did not expect test() to be called here.

--- config/test/config.ts
+++ config/test/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**'],
     globals: true,
     restoreMocks: true,
     environment: 'jsdom',
